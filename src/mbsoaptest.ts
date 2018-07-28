43b4cb9edafce9803acea9568a75b01d95a9b675
Barry Gausden <github@gausden.org>
2018-07-28 18:39:56 +0800

    EOD 20180728

--- src/mbsoaptest.ts
+++ src/mbsoaptest.ts
@@ -6,10 +6,9 @@ type TPageDetail = "Full"|"Basic"|"Bare";
 type MBSiteCall = "GetSites"|"GetLocations"|"GetActivationCode"|"GetPrograms"|"GetSessionTypes"|"GetResources"|"GetRelationships"|"GetGenders"|"GetProspectStages"|"GetMobileProviders";
 
 const MBAPIKey = "74b68c31704e40a69e9e60e1ef1de765";
-const MBSiteID = "-99";
 const midnight = new Date(new Date().setHours(0, 0, 0, 0));
 const defaultLocation = 0;
-const defaultSiteIDs = { int: -99 };   // siteids: { 'int': -99 }, this is what the soap library expects
+const defaultSiteID = -99;
 const defaultUserPassword = "apitest1234";
 const defaultUsername = "Siteowner";
 
@@ -18,42 +17,36 @@ const wsdlUrl = "https://api.mindbodyonline.com/0_5_1/SiteService.asmx?wsdl";
 const defaultUserCreds = {
   locationid: defaultLocation,
   password: defaultUserPassword,
-  siteids: defaultSiteIDs,
+  siteids: [defaultSiteID],
   username: defaultUsername,
 };
 
-const defaultSourceCreds = {
+// source credentials deprecated in MB API >= v5.0
+/* const defaultSourceCreds = {
   password: "oHmyTX0H/pciVoPW35pwahivDsE=",
   siteids: defaultSiteIDs,
   sourcename: "LissomeHongKongLimited",
   usercredentials: defaultUserCreds,
-};
-
-const defaultGetResourceParams: IGetResourcesParams = {
-  enddatetime: midnight,
-  locationid: 0,
-  sessiontypeid: 1,
-  startdatetime: midnight,
-};
+}; */
 
 const defaultPagingparams = {
   currentpageindex: 0,
   pagesize: 10,
-};
+}
 
 const defaultDetail: TPageDetail = "Full";
 
 interface IUserCredentials {
   username: string;
   password: string;
-  siteids: {int: number};
+  siteids: number[];
   locationid: number;
 }
 
 interface IMBSOAPUserCredentials {
   username: string;
   password: string;
-  siteids: { int: number };
+  siteids: number[];
   locationid: number;
 }
 
@@ -64,13 +57,26 @@ interface ISourceCredentials {
   usercredentials: IUserCredentials;
 }
 
+class CSessionTypeIDs {
+  constructor(typeIDs: number[]) {
+    return ({SessionTypeIDs: typeIDs})
+  }
+}
+
 interface IGetResourcesParams {
   locationid: number;
-  sessiontypeid: number;
+  sessiontypeids: CSessionTypeIDs;
   startdatetime: Date | string;
   enddatetime: Date | string;
   stringy?: () => IGetResourcesParams;
 }
+
+const defaultGetResourceParams: IGetResourcesParams = {
+  enddatetime: midnight,
+  locationid: 0,
+  sessiontypeids: {SessionTypeIDs: 1},
+  startdatetime: midnight,
+};
 
 interface IPagingParams {
   pagesize: number;
@@ -82,7 +88,7 @@ interface ISOAPGetResourcesArgs {
     SourceCredentials: ISourceCredentials,
     UserCredentials: IUserCredentials,
     LocationID: number,
-    SessionTypeIDs: number,
+    SessionTypeIDs: CSessionTypeIDs,
     StartDateTime: string,
     EndDateTime: string,
     XMLDetail: string,
@@ -92,57 +98,75 @@ interface ISOAPGetResourcesArgs {
 }
 
 class CUserCredentials implements IUserCredentials {
-  constructor(public username: string, public password: string,
-              public siteids: {"int": number}, public locationid: number) { }
-  public createMBSOAPUserCredentials(): IMBSOAPUserCredentials {
+  constructor(
+    public username: string, 
+    public password: string,
+    public siteids: number[], 
+    public locationid: number,
+  ) { };
+}
+
+/*   public createMBSOAPUserCredentials(): IMBSOAPUserCredentials {
     return {
       locationid: this.locationid,
       password: this.password,
       siteids: this.siteids,
       username: this.username,
+    }
+  } */
 
-    };
-  }
-}
 
 // tslint:disable-next-line:max-classes-per-file
-class CSourceCredentials implements ISourceCredentials {
-  constructor(public sourcename: string, public password: string, public siteids: { int: number },
-              public usercredentials: IUserCredentials, public LocationID: number,
-              public SessionTypeIDs: number, public StartDateTime: string, public EndDateTime: string) { }
-}
+// Source Credentials deprecated in API v5.0 and up
+/* class CSourceCredentials implements ISourceCredentials {
+  constructor(
+    public sourcename: string, 
+    public password: string, 
+    public siteids: { int: number },
+    public usercredentials: IUserCredentials, 
+    public LocationID: number,
+    public SessionTypeIDs: number, 
+    public StartDateTime: string, 
+    public EndDateTime: string) { }
+} */
 
 // tslint:disable-next-line:max-classes-per-file
 class CGetResourcesParams implements IGetResourcesParams {
-  constructor(public locationid: number, public sessiontypeid: number, public startdatetime: Date,
-              public enddatetime: Date) { }
+  constructor(
+    public locationid: number,
+    public sessiontypeids: CSessionTypeIDs, 
+    public startdatetime: Date,
+    public enddatetime: Date) { };
+
   public stringy(): IGetResourcesParams {
-    return {
+    return ({
       enddatetime: this.enddatetime.toJSON(),
       locationid: this.locationid,
-      sessiontypeid: this.sessiontypeid,
+      sessiontypeids: this.sessiontypeids,
       startdatetime: this.startdatetime.toJSON(),
-    };
+    });
   }
 }
 
 const testGRParams = new CGetResourcesParams(0, 1, midnight, midnight);
 
 // tslint:disable-next-line:max-classes-per-file
-class CGetResourcesArgs {
+/* class CGetResourcesArgs {
   constructor(public userCredentials: IUserCredentials, public sourceCredentials: ISourceCredentials,
               _getResourcesParam: IGetResourcesParams, _pagingParam: IPagingParams, _detail: TPageDetail) { }
 }
+ */
 
+/* 
+source credentials are deprecated in MB API >= v5.0
 const sourceCreds: ISourceCredentials = defaultSourceCreds;
+ */
 
 // tslint:disable-next-line:max-line-length
 const getResourcesParams: IGetResourcesParams = (new CGetResourcesParams(0, 1, midnight, midnight)).stringy();
 // const getResourcesParams: IGetResourcesParams = defaultGetResourceParams;
 const pagingParams: IPagingParams = defaultPagingparams;
-const getResourcesArgs: object = {
-  Request: Object.assign(sourceCreds, getResourcesParams, pagingParams),
-};
+const getResourcesArgs: object = {Request: Object.assign(getResourcesParams, pagingParams)};
 // const myCallback = (err: any, client: soap.Client): void => {console.log(err)};
 
 // const myCallback = (_err: any, client: soap.Client) => {client.GetResources(getResourcesArgs, (_err, _result, _rawResponse, _soapHeader, _rawRequest) => void)};
@@ -151,8 +175,8 @@ const getResourcesArgs: object = {
 const options: soap.IOptions = {
   disableCache: false,
   wsdl_headers: {
-    "API-key" : "74b68c31704e40a69e9e60e1ef1de765",
-    SiteID: "-99"
+    "API-key" : MBAPIKey,
+    SiteId: defaultSiteID,
     }
   };
 
@@ -162,10 +186,12 @@ const options: soap.IOptions = {
 
 // type ISOAPGetResourcesMethod = ((getResourcesArgs: object, callback: (err: any, result: any, raw: any, soapHeader: any) => void) => void);
 
-//here
-soap.createClient(wsdlUrl, options, (_err: any, client: soap.Client): void => {
+soap.createClient(wsdlUrl, (_err: any, client: soap.Client): void => {
+  client.addHttpHeader("API-key",MBAPIKey) ;
+  client.addHttpHeader("SiteId", defaultSiteID);
   const GetResources = client.GetResources as soap.ISoapMethod;
-  GetResources(getResourcesArgs, (_err, _result, _raw, _soapHeader): void => { console.log(client.lastRequest)})
+  GetResources(getResourcesArgs, (_err, _result, _raw, _soapHeader): void => {const siteDescription = client.describe();
+  console.log(`_result: ${JSON.stringify(_result,undefined,2)}`)})
 });
 
 // @ts-ignore TS2349
@@ -196,3 +222,4 @@ soap.createClient(wsdlUrl, options, (_err: any, client: soap.Client): void => {
   return date && Object.prototype.toString.call(date) === "[object Date]" && !isNaN(<any>date);
 } */
 console.log("Done");
+console.log();
