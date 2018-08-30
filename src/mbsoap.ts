673d30dd791907c3446d7a5dd1948f0cca00bca2
Barry Gausden <github@gausden.org>
2018-08-30 13:46:43 +0800

    Remove some wildcard imports

--- src/mbsoap.ts
+++ src/mbsoap.ts
@@ -1,17 +1,22 @@
+import { IGetResourcesParamsExternal, IGetSitesParamsExternal } from "./site";
+
+import { IGetScheduleItemsParamsExternal } from "./appointment";
+
+import { IGetStaffParamsExternal } from "./staff";
+
 // tslint:disable max-classes-per-file callable-types interface-over-type-literal
-import * as appointment from "./appointment";
-import * as site from "./site";
-import * as staff from "./staff";
 
 export type TSoapRequest = {
-    Request:
-        | site.IGetResourcesParamsExternal
-        | site.IGetSitesParamsExternal
-        | appointment.IGetScheduleItemsParamsExternal
-        | staff.IGetStaffParamsExternal
+  Request:
+    | IGetResourcesParamsExternal
+    | IGetSitesParamsExternal
+    | IGetScheduleItemsParamsExternal
+    | IGetStaffParamsExternal;
 };
 
-export function wrapRequest( params: site.IGetResourcesParamsExternal | appointment.IGetScheduleItemsParamsExternal): TSoapRequest {
-    // return an Object with one property "Request" whose value is the param Object
-    return { Request: params };
+export function wrapRequest(
+  params: IGetResourcesParamsExternal | IGetScheduleItemsParamsExternal
+): TSoapRequest {
+  // return an Object with one property "Request" whose value is the param Object
+  return { Request: params };
 }
