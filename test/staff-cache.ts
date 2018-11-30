ef1cb472303ecef6109c95f64411869dc1c11e35
Barry Gausden <barry.gausden@itiviti.com>
2018-11-30 12:07:12 +0800

    EOD 20181129

--- test/staff-cache.ts
+++ test/staff-cache.ts
@@ -1,8 +1,11 @@
-import {expect} from "chai";
+import { expect } from "chai";
 import "mocha";
 import { config } from "node-config-ts";
 import { ISoapServiceMethod } from "soap";
-import { IGetStaffResult, IStaff, IStaffMembers } from "../interfaces/mb_staff";
-import { makeRequest, setRequest } from "../request";
-import { TLocationIDs, TSiteID } from "../types/site";
-
+import {
+  IGetStaffResult,
+  IStaff,
+  IStaffMembers
+} from "../src/interfaces/mb_staff";
+import { makeRequest, setRequest } from "../src/request";
+import { TLocationIDs, TSiteID } from "../src/types/site";
